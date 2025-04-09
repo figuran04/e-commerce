1ca13c7993278e706320ad7a2d0e80ca05aefb0c
figuran04 <dikaelsaputra2406@gmail.com>
2025-04-09 10:50:40 +0700

    add: halaman home

--- views/home/index.php
+++ views/home/index.php
@@ -1,5 +1,10 @@
 <?php
 require '../../controllers/products/products_controller.php';
+
+$categories = [];
+while ($row = mysqli_fetch_assoc($categoriesResult)) {
+  $categories[] = $row;
+}
 $pageTitle = "Beranda";
 ob_start();
 ?>
@@ -8,20 +13,48 @@ ob_start();
   .product-grid{
      @apply grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4;
    }
-   h2 {
-     font-size: larger;
-     font-weight: bold;
-   }
- </style>
+</style>
 
-<h2>Produk Terbaru</h2>
+<img src="../../uploads/banner.png" alt="banner" class="rounded-lg w-full" />
+<div class="bg-white p-4 border border-gray-200 rounded-lg flex flex-col space-y-4">
+  <h1 class="text-xl font-bold">Kategori Pilihan</h1>
+  <div class="grid grid-cols-4 sm:grid-cols-6 md:grid-cols-8 gap-4">
+    <a href="../products"
+      class="border rounded px-4 py-1 aspect-square justify-center w-full flex items-end border-gray-200 line-clamp-2">
+      Semua
+    </a>
+    <?php
+    $limit = 7;
+    $count = 0;
+    foreach ($categories as $category) :
+      if ($count++ >= $limit) break;
+    ?>
+      <a href="../products?category=<?= $category['id'] ?>"
+        class="border rounded px-4 py-1 aspect-square justify-center w-full flex items-end border-gray-200 line-clamp-2">
+        <?= $category['name']; ?>
+      </a>
+    <?php endforeach; ?>
+  </div>
+  <div class="flex gap-4 overflow-x-scroll">
+    <a href="../products"
+      class="border rounded px-4 py-1 border-gray-200 text-lime-600">
+      Semua
+    </a>
+    <?php foreach ($categories as $category) : ?>
+      <a href="../products?category=<?= $category['id'] ?>"
+        class="border rounded px-4 py-1 border-gray-200 text-lime-600">
+        <?= $category['name']; ?>
+      </a>
+    <?php endforeach; ?>
+  </div>
+</div>
 
 <div class="product-grid">
   <?php include '../../includes/product_card.php'; ?>
 </div>
 
 <div class="w-full text-center">
-  <a href="../products">Lihat Semua</a>
+  <a href="../products" class="hover:text-lime-600 hover:underline">Lihat Semua</a>
 </div>
 
 <?php
